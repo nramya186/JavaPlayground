b66898ac4bee04b1913754d4a5f01e9bf636e595
Admin <nramya186@gmail.com>
2024-11-28 23:50:02 +0530

    section 7 arrays logic lab Logic Problems update

--- src/main/java/org/section7/arrays/logicLab/MatrixOperations.java
+++ src/main/java/org/section7/arrays/logicLab/MatrixOperations.java
@@ -9,6 +9,7 @@ public class MatrixOperations {
         //2.Populate the matrix with user input or hardcoded values.
         //3.Calculate and display the sum of all elements in the matrix.
         //4.Display the matrix in a grid format.
+/*
 
         System.out.println("1. Create a 3x3 matrix. ");
         int[][] matrix = new int[3][3];
@@ -34,6 +35,42 @@ public class MatrixOperations {
             }
         }
         System.out.println( "sum: "+ sum);
+
+*/
+
+        // Define two 3x3 matrices
+        int[][] matrix1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] matrix2 = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+
+        // Perform matrix addition
+         int additionResult[][]=new int[3][3];
+         for (int i=0;i<3;i++){
+             for (int j =0 ;j<3;j++){
+                 additionResult[i][j]=matrix1[i][j]+matrix2[i][j];
+                 System.out.print(additionResult[i][j] +" ");
+             }
+             System.out.println();
+         }
+        System.out.println("additionResult[][] "+ additionResult);
+
+        // Perform matrix subtraction
+        int subtractionResult[][]=new int[3][3];
+        for (int i=0;i<3;i++){
+            for (int j =0 ;j<3;j++){
+                subtractionResult[i][j]=matrix1[i][j]-matrix2[i][j];
+                System.out.print(subtractionResult[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
