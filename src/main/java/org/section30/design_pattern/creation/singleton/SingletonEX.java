93e777cbe44b946745762de64832d23d30af2e5b
Admin <nramya186@gmail.com>
2025-04-08 11:55:21 +0530

    add comments

--- src/main/java/org/section30/design_pattern/creation/singleton/SingletonEX.java
+++ src/main/java/org/section30/design_pattern/creation/singleton/SingletonEX.java
@@ -1,24 +1,30 @@
 package org.section30.design_pattern.creation.singleton;
 
 public class SingletonEX {
+    //1. create private static instance
     private static SingletonEX instance;
-    private SingletonEX(){
+    //2. create private constructor
+    private SingletonEX() {
         System.out.println("private constructor ");
     }
-    public static SingletonEX getInstance(){
-        if(instance==null){
+    //3. create public static method to get instance which return SingletonClass
+    public static SingletonEX getInstance() {
+        if (instance == null) {
             instance = new SingletonEX();
-        } return instance;
+        }
+        return instance;
     }
- public  void message1(String msg){
-     System.out.println("message "+msg);
- }
-
+    public void message1(String msg) {
+        System.out.println("message " + msg);
+    }
     public static void main(String[] args) {
-        SingletonEX singleton1= SingletonEX.getInstance();
-        SingletonEX singleton2= SingletonEX.getInstance();
-            singleton1.message1("instance 1");
-            singleton2.message1("instance 2");
-        System.out.println(singleton1==singleton2);
+        // Get two references to the Singleton instance
+        SingletonEX singleton1 = SingletonEX.getInstance();
+        SingletonEX singleton2 = SingletonEX.getInstance();
+        // Call message method using both references
+        singleton1.message1("instance 1");
+        singleton2.message1("instance 2");
+        // Check if both references point to the same object
+        System.out.println(singleton1 == singleton2); // should print true
     }
 }
